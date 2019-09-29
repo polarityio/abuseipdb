79a77edd0eb84973929c1995d627536c5191a7f7
Joseph Miller <joem@breachintelligence.com>
2019-09-29 08:35:58 -0400

    First version of integration. AbuseIPDB looks up IPs and returns user reported  information about them.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Polarity AbuseIPDB Integration
+
+The Polarity - AbuseIPDB Integration supports enrichment of IP addresses in ipv4 and ipv6 format. Enabling you to obtain AubuseIPDB information in real time.
+
+Check out the integration below:
+
+![](https://user-images.githubusercontent.com/22529325/65832590-bff3b300-e293-11e9-907b-5036e63a613f.png)
+
+To learn more about AbuseIPDB, please visit: [official website](https://www.abuseipdb.com)
+
+## AbuseIPDB Integration Options
+
+### AbuseIPDB ApiKey
+ApiKey needed to access the AbuseIPDB Api.
+
+### AbuseIPDB Max Age
+Max age in days to search AbuseIPDB.
+
+> Please note this muse be in day format ranging from 0-365.
+
+### Minimum Confidence Score
+Minimum score you want to be notified of from AbuseIPDB.
+
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ integration.js
@@ -0,0 +1,175 @@
+'use strict';
+
+let request = require('request');
+let _ = require('lodash');
+let util = require('util');
+let net = require('net');
+let config = require('./config/config');
+let async = require('async');
+let fs = require('fs');
+let Logger;
+let requestDefault;
+
+/**
+ *
+ * @param entities
+ * @param options
+ * @param cb
+ */
+function doLookup(entities, options, cb) {
+    let lookupResults = [];
+    let tasks = [];
+
+    Logger.trace({entities: entities}, 'entities');
+
+    entities.forEach(entity => {
+
+      if (entity.value) {
+        //do the lookup
+        let requestOptions = {
+          method: "GET",
+          uri: "https://api.abuseipdb.com/api/v2/check",
+          body: {
+              'maxAgeInDays': options.maxAge
+          },
+          headers: {
+              'Key': options.apiKey
+          },
+          qs: {
+              ipAddress: entity.value,
+          },
+          json: true
+        };
+
+            Logger.debug({uri: requestOptions}, 'Request URI');
+
+            tasks.push(function (done) {
+                requestDefault(requestOptions, function (error, res, body) {
+                    if(error){
+                        done({
+                            error: error,
+                            entity: entity.value,
+                            detail: "Error in Request"
+                        });
+                        return;
+                    }
+
+                    let result = {};
+                    if (res.statusCode === 200) {
+                        result = {
+                            entity: entity,
+                            body: body
+                        };
+                    } else if (res.statusCode === 429) {
+                        // reached rate limit
+                        error = "Reached API Lookup Limit";
+                    } else if (res.statusCode === 422) {
+                      // days excedded bounds
+                      error = "The max age in days must be between 1 and 365."
+                    } else {
+                        // Non 200 status code
+                        done({
+                            error: error,
+                            httpStatus: res.statusCode,
+                            body: body,
+                            detail: 'Unexpected Non 200 HTTP Status Code',
+                            entity: entity.value
+                        });
+                        return;
+                    }
+
+                    done(error, result);
+                });
+            });
+        }
+    });
+
+    async.parallelLimit(tasks, 10, (err, results) => {
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        results.forEach(result => {
+
+            if(result.body === null || _isMiss(result.body) || result.body.data.abuseConfidenceScore <= options.minScore){
+                lookupResults.push({
+                    entity:result.entity,
+                    data: null
+                });
+            } else {
+                lookupResults.push({
+                    entity: result.entity,
+                    data: {
+                        summary: [],
+                        details: result.body
+                    }
+                });
+            }
+        });
+
+        Logger.trace({lookupResults:lookupResults}, 'Lookup Results');
+
+        cb(null, lookupResults);
+    });
+}
+
+function _isMiss(body) {
+  if (
+    body &&
+    Array.isArray(body) &&
+    body.length === 0
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function startup(logger) {
+    Logger = logger;
+
+    let defaults = {};
+
+    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+        defaults.cert = fs.readFileSync(config.request.cert);
+    }
+
+    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+        defaults.key = fs.readFileSync(config.request.key);
+    }
+
+    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+        defaults.passphrase = config.request.passphrase;
+    }
+
+    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+        defaults.ca = fs.readFileSync(config.request.ca);
+    }
+
+    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+        defaults.proxy = config.request.proxy;
+    }
+
+    requestDefault = request.defaults(defaults);
+}
+
+function validateOptions(userOptions, cb) {
+  let errors = [];
+  if (
+    typeof userOptions.apiKey.value !== "string" ||
+    (typeof userOptions.apiKey.value === "string" &&
+      userOptions.apiKey.value.length === 0)
+  ) {
+    errors.push({
+      key: "apiKey",
+      message: "You must provide a valid AbuseIPDB API key"
+    });
+  }
+  cb(null, errors);
+}
+
+module.exports = {
+    doLookup: doLookup,
+    validateOptions: validateOptions,
+    startup: startup
+};
