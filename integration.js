8dba090457c7ffb87aba7533c0810950b8c30482
Ed <ed@polarity.io>
2019-09-30 10:03:36 -0400

    Reformat with prettier

--- integration.js
+++ integration.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const request = require('request');
-const config = require('./config/config');
-const async = require('async');
-const fs = require('fs');
+const request = require("request");
+const config = require("./config/config");
+const async = require("async");
+const fs = require("fs");
 
 let Logger;
 let requestDefault;
@@ -15,140 +15,147 @@ let requestDefault;
  * @param cb
  */
 function doLookup(entities, options, cb) {
-    let lookupResults = [];
-    let tasks = [];
+  let lookupResults = [];
+  let tasks = [];
 
-    Logger.trace({entities: entities}, 'entities');
+  Logger.trace({ entities: entities }, "entities");
 
-    entities.forEach(entity => {
+  entities.forEach(entity => {
+    if (entity.value) {
+      //do the lookup
+      let requestOptions = {
+        method: "GET",
+        uri: "https://api.abuseipdb.com/api/v2/check",
+        body: {
+          maxAgeInDays: options.maxAge
+        },
+        headers: {
+          Key: options.apiKey
+        },
+        qs: {
+          ipAddress: entity.value
+        },
+        json: true
+      };
 
-      if (entity.value) {
-        //do the lookup
-        let requestOptions = {
-          method: "GET",
-          uri: "https://api.abuseipdb.com/api/v2/check",
-          body: {
-              'maxAgeInDays': options.maxAge
-          },
-          headers: {
-              'Key': options.apiKey
-          },
-          qs: {
-              ipAddress: entity.value,
-          },
-          json: true
-        };
+      Logger.debug({ uri: requestOptions }, "Request URI");
 
-            Logger.debug({uri: requestOptions}, 'Request URI');
-
-            tasks.push(function (done) {
-                requestDefault(requestOptions, function (error, res, body) {
-                    if(error){
-                        done({
-                            error: error,
-                            entity: entity.value,
-                            detail: "Error in Request"
-                        });
-                        return;
-                    }
-
-                    let result = {};
-                    if (res.statusCode === 200) {
-                        result = {
-                            entity: entity,
-                            body: body
-                        };
-                    } else if (res.statusCode === 429) {
-                        // reached rate limit
-                        error = "Reached API Lookup Limit";
-                    } else if (res.statusCode === 422) {
-                      // days excedded bounds
-                      error = "The max age in days must be between 1 and 365."
-                    } else {
-                        // Non 200 status code
-                        done({
-                            error: error,
-                            httpStatus: res.statusCode,
-                            body: body,
-                            detail: 'Unexpected Non 200 HTTP Status Code',
-                            entity: entity.value
-                        });
-                        return;
-                    }
-
-                    done(error, result);
-                });
+      tasks.push(function(done) {
+        requestDefault(requestOptions, function(error, res, body) {
+          if (error) {
+            done({
+              error: error,
+              entity: entity.value,
+              detail: "Error in Request"
             });
-        }
-    });
-
-    async.parallelLimit(tasks, 10, (err, results) => {
-        if (err) {
-            cb(err);
             return;
-        }
+          }
 
-        results.forEach(result => {
+          let result = {};
+          if (res.statusCode === 200) {
+            result = {
+              entity: entity,
+              body: body
+            };
+          } else if (res.statusCode === 429) {
+            // reached rate limit
+            error = "Reached API Lookup Limit";
+          } else if (res.statusCode === 422) {
+            // days excedded bounds
+            error = "The max age in days must be between 1 and 365.";
+          } else {
+            // Non 200 status code
+            done({
+              error: error,
+              httpStatus: res.statusCode,
+              body: body,
+              detail: "Unexpected Non 200 HTTP Status Code",
+              entity: entity.value
+            });
+            return;
+          }
 
-            if(result.body === null || _isMiss(result.body) || result.body.data.abuseConfidenceScore <= options.minScore){
-                lookupResults.push({
-                    entity:result.entity,
-                    data: null
-                });
-            } else {
-                lookupResults.push({
-                    entity: result.entity,
-                    data: {
-                        summary: [],
-                        details: result.body
-                    }
-                });
-            }
+          done(error, result);
         });
+      });
+    }
+  });
 
-        Logger.trace({lookupResults:lookupResults}, 'Lookup Results');
+  async.parallelLimit(tasks, 10, (err, results) => {
+    if (err) {
+      cb(err);
+      return;
+    }
 
-        cb(null, lookupResults);
+    results.forEach(result => {
+      if (
+        result.body === null ||
+        _isMiss(result.body) ||
+        result.body.data.abuseConfidenceScore <= options.minScore
+      ) {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      } else {
+        lookupResults.push({
+          entity: result.entity,
+          data: {
+            summary: [],
+            details: result.body
+          }
+        });
+      }
     });
+
+    Logger.trace({ lookupResults: lookupResults }, "Lookup Results");
+
+    cb(null, lookupResults);
+  });
 }
 
 function _isMiss(body) {
-  if (
-    body &&
-    Array.isArray(body) &&
-    body.length === 0
-  ) {
+  if (body && Array.isArray(body) && body.length === 0) {
     return true;
   }
   return false;
 }
 
 function startup(logger) {
-    Logger = logger;
+  Logger = logger;
 
-    let defaults = {};
+  let defaults = {};
 
-    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
-        defaults.cert = fs.readFileSync(config.request.cert);
-    }
+  if (
+    typeof config.request.cert === "string" &&
+    config.request.cert.length > 0
+  ) {
+    defaults.cert = fs.readFileSync(config.request.cert);
+  }
 
-    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
-        defaults.key = fs.readFileSync(config.request.key);
-    }
+  if (typeof config.request.key === "string" && config.request.key.length > 0) {
+    defaults.key = fs.readFileSync(config.request.key);
+  }
 
-    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
-        defaults.passphrase = config.request.passphrase;
-    }
+  if (
+    typeof config.request.passphrase === "string" &&
+    config.request.passphrase.length > 0
+  ) {
+    defaults.passphrase = config.request.passphrase;
+  }
 
-    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
-        defaults.ca = fs.readFileSync(config.request.ca);
-    }
+  if (typeof config.request.ca === "string" && config.request.ca.length > 0) {
+    defaults.ca = fs.readFileSync(config.request.ca);
+  }
 
-    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
-        defaults.proxy = config.request.proxy;
-    }
+  if (
+    typeof config.request.proxy === "string" &&
+    config.request.proxy.length > 0
+  ) {
+    defaults.proxy = config.request.proxy;
+  }
 
-    requestDefault = request.defaults(defaults);
+  requestDefault = request.defaults(defaults);
 }
 
 function validateOptions(userOptions, cb) {
@@ -167,7 +174,7 @@ function validateOptions(userOptions, cb) {
 }
 
 module.exports = {
-    doLookup: doLookup,
-    validateOptions: validateOptions,
-    startup: startup
+  doLookup: doLookup,
+  validateOptions: validateOptions,
+  startup: startup
 };
